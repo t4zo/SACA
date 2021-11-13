eac6962a080f819bb4af12c2e33bd7b9f9cf7386
Tacio de Souza Campos <taciodesouzacampos@gmail.com>
2021-11-13 09:39:46 -0300

    adding digital-ocean workflow and base route

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SACA.Controllers
+{
+    public class HomeController
+    {
+        [AllowAnonymous]
+        [HttpGet("/")]
+        public ActionResult<string> Index()
+        {
+            return "SACA";
+        }
+    }
+}
